7167822ce888e15755b87f3c80a3f8656633bfd3
Enrico Buonanno <enrico.buonanno@gmail.com>
2021-03-18 10:37:50 +0100

    added ext to covnert Nullable to Option

---
+++ LaYumba.Functional/Nullable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaYumba.Functional
+{
+   using static F;
+
+   public static class NullableExt
+   {
+      public static Option<T> ToOption<T>(this Nullable<T> @this) where T : struct
+         => @this.HasValue ? Some(@this.Value) : None;
+   }
+}
